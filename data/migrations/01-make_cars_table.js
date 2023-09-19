200edc1512d22fecc58ec7009818c14d4a059033
Rafaela Serrano <raphaela.alonsoserrano@gmail.com>
2023-09-19 09:13:06 -0500

    format up function in 01-make_cars_table.js

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,6 +1,12 @@
 exports.up = async function (knex) {
   await knex.schema.createTable('cars', table =>{
-    
+    table.increments()
+    table.string('vin', 17).notNullable().unique()
+    table.string('make', 128).notNullable()
+    table.string('model', 128).notNullable()
+    table.numeric('mileage').unsigned().notNullable()
+    table.string('title', 128).defaultTo('not provided')
+    table.string('transmission', 128)
   })
 };
 
